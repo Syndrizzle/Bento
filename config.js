6cb177275044f72ae090b65abe700deb7e96bec6
Sahil Nihalani <96112833+Syndrizzle@users.noreply.github.com>
2022-10-18 09:54:22 +0530

    Dont need location

--- config.js
+++ config.js
@@ -34,7 +34,7 @@ const CONFIG = {
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
-	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
+	trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
 	defaultLatitude: '26.449',
 	defaultLongitude: '74.639',
 
