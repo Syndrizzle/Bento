cc6235957a73ea3599ee299587a65693c6bcb0c6
Sahil Nihalani <96112833+Syndrizzle@users.noreply.github.com>
2022-12-11 20:05:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -39,10 +39,10 @@ const CONFIG = {
 	defaultLongitude: '74.639',
 
 	// Autochange
-	autoChangeTheme: true,
+	autoChangeTheme: false,
 
 	// Autochange by OS
-	changeThemeByOS: true,
+	changeThemeByOS: false,
 
 	// Autochange by hour options (24hrs format, string must be in: hh:mm)
 	changeThemeByHour: false,
